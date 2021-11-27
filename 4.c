5de7eafe615ce43819a7af7f07f039865c5fc4ea
Kruti2673 <krutivadaliya01@gmail.com>
2021-11-27 13:07:50 +0530

    4th added

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () 
+{
+    int a;
+    printf("enetr a:");
+    scanf("%d",&a);
+    if(a>0)
+        printf("no. is positive:\n",a);
+    else
+        printf("no. is negative:\n",a);
+    return 0;            
+}  
